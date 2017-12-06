36053ac8a5c5330c0c5b2317d355c0fd37c109a6
MKendall17 <mkendall@stuy.edu>
2017-12-05 21:59:03 -0500

    modified Warrior toString

--- v2/ProtagonistClasses/Warrior.java
+++ v2/ProtagonistClasses/Warrior.java
@@ -1,6 +1,12 @@
 public class Warrior extends Protagonist {
+	protected int swordSlices;
 
-	public Warrior( String name ){
-	super(name);
+	public Warrior( String name , int pswordSlices){
+		super(name);
+		swordSlices = pswordSlices;
     }
+
+    public String toString() {
+      return super.toString(); + "SPECIAL MOVE: Can slice" + swordSlices + " times in one go";
+    }  
 }
