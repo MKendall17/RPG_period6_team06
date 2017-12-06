cf16fbf38d7ad6cb700b974f0f719967211e51d2
MKendall17 <mkendall@stuy.edu>
2017-12-05 20:53:27 -0500

    added directories for Monster Classes and Protagonist Classes

--- v2/Monster.java
+++
@@ -1,8 +0,0 @@
-public class Monster extends Character {
-    public Monster() {
-        health = 250;
-        strength = (int)(20 + 40 * Math.random());
-        defense = 40;
-        rating = 0.3;
-    }
-}
