6835440b21a85458342fcaac3c83884ad368a382
lbushra <34113646+lbushra@users.noreply.github.com>
2017-12-05 21:33:42 -0500

    Update Archer.java

--- v2/ProtagonistClasses/Archer.java
+++ v2/ProtagonistClasses/Archer.java
@@ -1,4 +1,15 @@
 public class Archer extends Protagonist {
-
-
+  private 
+    public Archer() {
+        super(name);
+    }
+  
+    public void normalize() {
+        this.defense = 30;
+        this.attack = 50;
+    }
+  
+    public String toString() {
+      super.toString();
+    }  
 }
